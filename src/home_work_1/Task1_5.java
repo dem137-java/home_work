784c3dae1ea33d4840b4a124207f270605581560
Dmitry Demyanchik <d.demyanchik@gmail.com>
2022-03-25 11:56:43 +0300

    добавлено Task1_5.java

---
+++ src/home_work_1/Task1_5.java
@@ -0,0 +1,15 @@
+package home_work_1;
+
+public class Task1_5 {
+    public static void main(String[] args) {
+        double a = 42.50;
+
+        float b = 42.50f;
+        //float c = b>>2;
+
+        //System.out.println(~a);
+        //System.out.println(c);
+    }
+}
+//Результат - код не компилируется, поскольку побитовые операторы могут быть применены только к целочисленным типам
+//int, long, short, char, byte
