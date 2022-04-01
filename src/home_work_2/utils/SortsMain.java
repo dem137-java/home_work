0001fb2e498ffcadb0d05a74baf5f5a8c09bd828
d.demyanchik <d.demyanchik@gmail.com>
2022-04-01 22:08:37 +0300

    скорректированно задание 4.2

--- src/home_work_2/utils/SortsMain.java
+++ src/home_work_2/utils/SortsMain.java
@@ -7,92 +7,85 @@ public class SortsMain {
 
         //тестируем пузырьковую сортировку
         int[] array1 = new int[]{1, 2, 3, 4, 5, 6};
-        printInitArray(array1);
-        SortsUtils.bubble(array1);
-        printSortedArray(array1);
+        testBubbleSort(array1);
 
         int[] array2 = new int[]{1, 1, 1, 1};
-        printInitArray(array2);
-        SortsUtils.bubble(array2);
-        printSortedArray(array2);
+        testBubbleSort(array2);
 
         int[] array3 = new int[]{9, 1, 5, 99, 9, 9};
-        printInitArray(array3);
-        SortsUtils.bubble(array3);
-        printSortedArray(array3);
+        testBubbleSort(array3);
 
         int[] array4 = new int[]{};
-        printInitArray(array4);
-        SortsUtils.bubble(array4);
-        printSortedArray(array4);
+        testBubbleSort(array4);
 
         int[] array5 = new int[]{6, 5, 4, 3, 2, 1};
-        printInitArray(array5);
-        SortsUtils.bubble(array5);
-        printSortedArray(array5);
+        testBubbleSort(array5);
 
         int[] array6 = ArraysUtils.arrayRandom(50, 100);
-        printInitArray(array6);
-        SortsUtils.bubble(array6);
-        printSortedArray(array6);
+        testBubbleSort(array6);
 
         int[] array7 = ArraysUtils.arrayFromConsole();
-        printInitArray(array7);
-        SortsUtils.bubble(array7);
-        printSortedArray(array7);
+        testBubbleSort(array7);
 
         //тестируем шейкерную сортировку
         array1 = new int[]{1, 2, 3, 4, 5, 6};
-        printInitArray(array1);
-        SortsUtils.shake(array1);
-        printSortedArray(array1);
+        testShakerSort(array1);
 
         array2 = new int[]{1, 1, 1, 1};
-        printInitArray(array2);
-        SortsUtils.shake(array2);
-        printSortedArray(array2);
+        testShakerSort(array2);
 
         array3 = new int[]{9, 1, 5, 99, 9, 9};
-        printInitArray(array3);
-        SortsUtils.shake(array3);
-        printSortedArray(array3);
+        testShakerSort(array3);
 
         array4 = new int[]{};
-        printInitArray(array4);
-        SortsUtils.shake(array4);
-        printSortedArray(array4);
+        testShakerSort(array4);
 
         array5 = new int[]{6, 5, 4, 3, 2, 1};
-        printInitArray(array5);
-        SortsUtils.shake(array5);
-        printSortedArray(array5);
+        testShakerSort(array5);
 
         array6 = ArraysUtils.arrayRandom(50, 100);
-        printInitArray(array6);
-        SortsUtils.shake(array6);
-        printSortedArray(array6);
+        testShakerSort(array6);
 
         array7 = ArraysUtils.arrayFromConsole();
-        printInitArray(array7);
-        SortsUtils.bubble(array7);
-        printSortedArray(array7);
+        testShakerSort(array7);
     }
 
-// ниже 2 метода для распечатки массивов в заданном формате
-    public static void printInitArray(int[] arr) {
+    /**
+     * метод сортирует переданный массив пузырьковым методом и выводит результат сортировки в заданном формате
+     * [Какой массив был до сортировки] -> [Каким стал массив после сортировки]
+     * @param arr - одномерный массив значений типа int
+     */
+    public static void testBubbleSort(int[] arr) {
         System.out.print("[ ");
         for (int i : arr) {
             System.out.print(i + " ");
         }
         System.out.print("] -> ");
-    }
-    public static void printSortedArray(int[] arr) {
+        SortsUtils.bubble(arr);
         System.out.print("[ ");
         for (int i : arr) {
             System.out.print(i + " ");
         }
-        System.out.println("] ");
+        System.out.println("]");
+    }
 
-
+    /**
+     * метод сортирует переданный массив шейкерным методом и выводит результат сортировки в заданном формате
+     * [Какой массив был до сортировки] -> [Каким стал массив после сортировки]
+     * @param arr - одномерный массив значений типа int
+     */
+    public static void testShakerSort(int[] arr) {
+        System.out.print("[ ");
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.print("] -> ");
+        SortsUtils.shake(arr);
+        System.out.print("[ ");
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println("]");
     }
 }
+
