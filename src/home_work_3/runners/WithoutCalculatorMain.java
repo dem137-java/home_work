4ae9edfe14cf2309c54cc665b45bfd694237c636
Dmitry Demyanchik <d.demyanchik@gmail.com>
2022-04-10 01:30:23 +0300

    git commit -a -m "добавлено задание 1"

---
+++ src/home_work_3/runners/WithoutCalculatorMain.java
@@ -0,0 +1,8 @@
+package home_work_3.runners;
+
+public class WithoutCalculatorMain {
+    public static void main(String[] args) {
+        double result = 4.1 + 15 * 7 + Math.pow(28.00 / 5.00,2);
+        System.out.println(result);
+    }
+}
