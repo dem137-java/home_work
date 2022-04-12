eb58f2b3704b5e7f8b34a26b1ee55ab0f6151266
d.demyanchik <d.demyanchik@gmail.com>
2022-04-12 10:44:48 +0300

    добавлено задание 8

---
+++ src/home_work_3/calcs/additional/CalculatorWithCounterAutoAgregationInterface.java
@@ -0,0 +1,90 @@
+package home_work_3.calcs.additional;
+
+import home_work_3.calcs.api.ICalculator;
+
+public class CalculatorWithCounterAutoAgregationInterface {
+    private long countOperations;
+    private ICalculator calc;
+
+    public CalculatorWithCounterAutoAgregationInterface(ICalculator someCalc){
+        calc = someCalc;
+    }
+    public long getCountOperations() {
+        return countOperations;
+    }
+
+    /**
+     * метод возвращает результат деления числа a на число b
+     * @param a число a
+     * @param b число b
+     * @return результат деления
+     */
+    public double divide(double a, double b) {
+        countOperations++;
+        return calc.divide(a,b);
+    }
+
+    /**
+     * метод возвращает результат умножения числа a на число b
+     * @param a число a
+     * @param b число b
+     * @return результат умножения
+     */
+    public double multiply(double a, double b) {
+        countOperations++;
+        return calc.multiply(a,b);
+    }
+
+    /**
+     * метод возвращает результат сложения числа a и числа b
+     * @param a число a
+     * @param b число b
+     * @return результат сложения
+     */
+    public double add(double a, double b) {
+        countOperations++;
+        return calc.add(a,b);
+    }
+
+    /**
+     * метод возвращает результат вычитания числа b из числа a
+     * @param a число a
+     * @param b число b
+     * @return результат вычитания
+     */
+    public double deduct(double a, double b) {
+        countOperations++;
+        return calc.deduct(a,b);
+    }
+
+    /**
+     * метод возвращает результат возведения в степень b положительного числа a
+     * @param a число a
+     * @param b степень
+     * @return результат возведения в степень
+     */
+    public double pow(double a, int b) {
+        countOperations++;
+        return calc.pow(a,b);
+    }
+
+    /**
+     * метод возвращает модуль числа a
+     * @param a число a
+     * @return модуль числа а
+     */
+    public double abs(double a) {
+        countOperations++;
+        return calc.abs(a);
+    }
+
+    /**
+     * метод возвращает квадратный корень из числа a (без использования библиотеки Math)
+     * @param a число a
+     * @return квадратный корень из числа a
+     */
+    public double sqrt(double a){
+        countOperations++;
+        return calc.sqrt(a);
+    }
+}
---
+++ src/home_work_3/runners/CalculatorWithCounterAutoCompositeInterfaceMain.java
@@ -0,0 +1,13 @@
+package home_work_3.runners;
+
+import home_work_3.calcs.additional.CalculatorWithCounterAutoAgregationInterface;
+import home_work_3.calcs.simple.CalculatorWithMathCopy;
+
+public class CalculatorWithCounterAutoCompositeInterfaceMain {
+    public static void main(String[] args) {
+        String toCalc = "4.1 + 15 * 7 + (28 / 5) ^ 2 = ";
+        CalculatorWithCounterAutoAgregationInterface calc = new CalculatorWithCounterAutoAgregationInterface(new CalculatorWithMathCopy());
+        double result = calc.add(4.1,(calc.add(calc.multiply(15,7), calc.pow(28.0/5,2))));
+        System.out.println(toCalc + result);
+    }
+}
