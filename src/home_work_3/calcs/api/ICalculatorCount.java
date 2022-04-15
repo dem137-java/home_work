c2cbf88e1311fbab46d2423fc0340b655d3cc032
Dmitry Demyanchik <d.demyanchik@gmail.com>
2022-04-15 23:59:28 +0300

    git commit -a -m "попытка создания 2 расширенных интерфейсов"

---
+++ src/home_work_3/calcs/api/ICalculatorCount.java
@@ -0,0 +1,7 @@
+package home_work_3.calcs.api;
+
+public interface ICalculatorCount extends ICalculator {
+    public long getCountOperations();
+    public ICalculator getCalculator();
+
+}
---
+++ src/home_work_3/calcs/api/ICalculatorMemory.java
@@ -0,0 +1,6 @@
+package home_work_3.calcs.api;
+
+public interface ICalculatorMemory extends ICalculatorCount {
+    public double getMemory();
+    public void setMemory();
+}
