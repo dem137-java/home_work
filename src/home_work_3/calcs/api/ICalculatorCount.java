2be6fddff34d26ad1c2ea54909e7759866730365
Dmitry Demyanchik <d.demyanchik@gmail.com>
2022-04-25 23:03:00 +0300

    удалены лишние файлы ICalculatorCount.java и ICalculatorMemory.java

--- src/home_work_3/calcs/api/ICalculatorCount.java
+++
@@ -1,7 +0,0 @@
-package home_work_3.calcs.api;
-
-public interface ICalculatorCount extends ICalculator {
-    public long getCountOperations();
-    public ICalculator getCalculator();
-
-}
--- src/home_work_3/calcs/api/ICalculatorMemory.java
+++
@@ -1,6 +0,0 @@
-package home_work_3.calcs.api;
-
-public interface ICalculatorMemory extends ICalculatorCount {
-    public double getMemory();
-    public void setMemory();
-}
