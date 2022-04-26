320f9749817f52fc11c907e0d528f7758613aacc
d.demyanchik <d.demyanchik@gmail.com>
2022-04-26 15:54:24 +0300

    "в процессе решения задачи про студента (коллекции)"

--- src/home_work_4/taskStudent/NameComparator.java
+++ src/home_work_4/taskStudent/NameComparator.java
@@ -9,9 +9,9 @@ public class NameComparator implements Comparator<Student> {
         int result = 0;
         int minLength = Math.min(o1.getName().length(), o2.getName().length());
         char[] name1 = o1.getName().toCharArray();
-        char[] name2 = o1.getName().toCharArray();
+        char[] name2 = o2.getName().toCharArray();
         for (int i = 0; i < minLength; i++) {
-            if (name1[i] != name2[i]) {
+            if ((int) name1[i]!=(int) name2[i]) {
                 result = (int) name1[i] - (int) name2[i];
                 break;
             }
