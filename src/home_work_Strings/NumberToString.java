2af7543cdb2f4e0e98db70cb7414cf08575f0ab0
Dmitry Demyanchik <d.demyanchik@gmail.com>
2022-05-09 23:52:06 +0300

    скорректировано задание 1

--- src/home_work_Strings/NumberToString.java
+++ src/home_work_Strings/NumberToString.java
@@ -1,12 +1,12 @@
 package home_work_Strings;
 
+/**
+ * класс предназначен для получения прописью числа из диапазона от -999999999 до 999999999
+ */
 public class NumberToString {
     private int underThousand;
     private int thousand;
     private int million;
-    private int hundred;
-    private int ten;
-    private int unit;
 
     private final String[][] figures = {
             {"", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять"},
@@ -18,6 +18,11 @@ public class NumberToString {
     private final String[] wordsMillionArray = {"миллион", "миллиона", "миллионов"};
     private final String[] wordsThousandArray = {"тысяча", "тысячи", "тысяч"};
 
+    /**
+     * метод для получения числа прописью
+     * @param number число для конвертации в строку
+     * @return строка содержащая число прописью
+     */
     public String toString(int number) {
         String result = "";
         if (number > 999999999 || number < -999999999) {
@@ -31,15 +36,24 @@ public class NumberToString {
             number = -number;
         }
         //разбиваем число на разряды (по 3 цифры)
-        million = (int) number / 1_000_000;
-        thousand = (int) (number - (million * 1_000_000)) / 1000;
-        underThousand = (int) (number - (million * 1_000_000)) % 1000;
-
+        million = number / 1_000_000;
+        thousand = (number - (million * 1_000_000)) / 1000;
+        underThousand = (number - (million * 1_000_000)) % 1000;
         return result += threeFigToString(million, 0) + threeFigToString(thousand, 1) + threeFigToString(underThousand, 2);
     }
 
+    /**
+     * метод предназначен для получения прописью числа, состоящего из трех цифр
+     * @param i сегмент числа из 3-х цифр
+     * @param index индекс для определения разряда (0-миллионы,1-тысячи)
+     * @return строка содержащая число прописью с указанием разряда
+     */
     private String threeFigToString(int i, int index) {
-        String result = "";
+        String subResult = "";
+        int hundred;
+        int ten;
+        int unit;
+
         if (i == 0) {
             return "";
         } else {
@@ -49,55 +63,54 @@ public class NumberToString {
             unit = (i - hundred * 100) % 10;
 
             //добавляем сотни
-            result += hundredsArray[hundred];
+            subResult += hundredsArray[hundred];
             if (hundred!=0&&(ten!=0||unit!=0)){
-                result+=" ";
+                subResult+=" ";
             }
-            //формируем и добавляем десятки
+            //формируем и добавляем десятки и единицы
             if (ten == 1 && unit == 0) {
-                result += dozensArray[ten];
+                subResult += dozensArray[ten];
             }
             if (ten == 1 && unit != 0) {
-                result += elevensArray[unit];
+                subResult += elevensArray[unit];
             }
             if (ten != 1) {
-                result += dozensArray[ten];
+                subResult += dozensArray[ten];
                 if (ten!=0&&unit!=0){
-                    result+=" ";
+                    subResult+=" ";
                 }
                 if (index == 1) {
-                    result += figures[1][unit];
+                    subResult += figures[1][unit];
                 } else {
-                    result += figures[0][unit];
+                    subResult += figures[0][unit];
                 }
             }
-
             //добавляем слова разрядов - миллионы и тысячи
             if (million!=0&&index == 0) {
                 if (unit == 1) {
-                    result += " " + wordsMillionArray[0];
+                    subResult += " " + wordsMillionArray[0];
                 } else if (unit == 2 | unit == 3 | unit == 4) {
-                    result += " " + wordsMillionArray[1];
+                    subResult += " " + wordsMillionArray[1];
                 } else {
-                    result += " " + wordsMillionArray[2];
+                    subResult += " " + wordsMillionArray[2];
                 }
                 if (thousand!=0){
-                    result+= " ";
+                    subResult+= " ";
                 }
             }
             if (thousand!=0&&index == 1) {
                 if (unit == 1) {
-                    result += " " + wordsThousandArray[0];
+                    subResult += " " + wordsThousandArray[0];
                 } else if (unit == 2 | unit == 3 | unit == 4) {
-                    result += " " + wordsThousandArray[1];
+                    subResult += " " + wordsThousandArray[1];
                 } else {
-                    result += " " + wordsThousandArray[2];
+                    subResult += " " + wordsThousandArray[2];
                 }
                 if (underThousand!=0){
-                    result+= " ";
+                    subResult+= " ";
                 }
             }
-            return result;
+            return subResult;
         }
     }
 }
