1d9fa868ab64867066b176afe4910af9c1e6d1e4
Dmitry Demyanchik <d.demyanchik@gmail.com>
2022-05-11 22:02:02 +0300

    в процессе задание 1

--- src/home_work_5/WarAndPeace.java
+++ src/home_work_5/WarAndPeace.java
@@ -1,36 +1,39 @@
 package home_work_5;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Scanner;
 
 public class WarAndPeace {
-    String fileName = "\\upload\\Java\\projects\\JD1\\home_work\\src\\home_work_5\\text.txt";
-    String textFull;
 
-    public String readFileBufferedReader() throws IOException {
+    public String readFileBufferedReader(String fileName) throws IOException {
         BufferedReader in = new BufferedReader(new FileReader(fileName));
         StringBuilder builder = new StringBuilder();
-        while (in.readLine() != null) {
-            builder.append(in.readLine()).append("\n");
-        }
-        textFull = builder.toString();
+        String str = null;
+        do {
+            str = in.readLine();
+            if(str != null){
+                builder.append(str).append("\n");
+            }
+        } while (str != null);
         in.close();
-        return textFull;
+        return builder.toString();
     }
 
-    public String readFileScanner() throws FileNotFoundException {
+    public String readFileScanner(String fileName) throws FileNotFoundException {
         File file = new File(fileName);
         Scanner scanner = new Scanner(file);
         StringBuilder builder = new StringBuilder();
-
         while(scanner.hasNextLine()) {
             builder.append(scanner.nextLine()).append("\n");
         }
-        textFull = builder.toString();
-        return textFull;
+        return builder.toString();
     }
 
-    public WarAndPeace() throws IOException {
+    public String readFileStrings(String fileName) throws IOException {
+        Path filePath = Path.of(fileName);
+        return Files.readString(filePath);
     }
 }
 
--- src/home_work_5/WarAndPeaceTest.java
+++ src/home_work_5/WarAndPeaceTest.java
@@ -7,21 +7,28 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WarAndPeaceTest {
+    private String fileName = "\\JAVA\\Projects\\JD1\\HomeWork\\src\\home_work_5\\text.txt";
+
     @Test
     public void readFileBufferedReader() throws IOException {
         WarAndPeace book = new WarAndPeace();
-        System.out.println(book.readFileBufferedReader());
-        File newFile = new File("\\upload\\Java\\projects\\JD1\\home_work\\src\\home_work_5\\NewFile.txt");
+        System.out.println(book.readFileBufferedReader(fileName));
+        File newFile = new File("\\JAVA\\Projects\\JD1\\HomeWork\\src\\home_work_5\\NewFile.txt");
         FileWriter writer = new FileWriter(newFile,false);
-        writer.write(book.readFileBufferedReader());
+        writer.write(book.readFileBufferedReader(fileName));
     }
 
     @Test
     public void readFileScanner() throws IOException {
         WarAndPeace book = new WarAndPeace();
-        System.out.println(book.readFileScanner());
-        File newFile = new File("\\upload\\Java\\projects\\JD1\\home_work\\src\\home_work_5\\NewFile2.txt");
+        System.out.println(book.readFileScanner(fileName));
+        File newFile = new File("\\JAVA\\Projects\\JD1\\HomeWork\\src\\home_work_5\\NewFile2.txt");
         FileWriter writer = new FileWriter(newFile,false);
-        writer.write(book.readFileScanner());
+        writer.write(book.readFileScanner(fileName));
+    }
+    @Test
+    public void readFilesString () throws IOException {
+        WarAndPeace book = new WarAndPeace();
+        System.out.println(book.readFileStrings(fileName));
     }
 }
